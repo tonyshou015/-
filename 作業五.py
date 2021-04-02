f4763642189538d3ff43798c165b86c7dfadbc1f
Tony <tonyshou1000@gmail.com>
2021-04-02 12:06:09 +0800

    2.0

--- 作業五.py
+++ 作業五.py
@@ -1,6 +1,6 @@
-year=input("請輸入一個年份:")
+year=eval(input("請輸入一個年份:"))
 
-if year==(year%400 == 0) or ((year%4 == 0) and (year%100 != 0)):
+if (year%400 == 0) or ((year%4 == 0) and (year%100 != 0)):
     print("閨年")
     
 
