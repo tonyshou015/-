95a78cb6b077442b2b29537330428aec79cadb9e
Tony <tonyshou1000@gmail.com>
2021-04-04 00:01:53 +0800

    3.0

--- 作業7.py
+++ 作業7.py
@@ -1,15 +1,15 @@
 x=eval(input("請輸入 n值，我幫您算n階乘:"))
 
 if x!=0:
-    
+    e=x
     z=(x-1)
     while z>0:
-    
+        
         x=x*z
         z-=1
   
     else:
-        print(x)
+        print("{0}!={1}".format(e, x))
         
     if x<0:
         print("不符合輸入值，n應該大於等於0")
