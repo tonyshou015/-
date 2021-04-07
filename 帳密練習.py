e2efb0a54415cd554f643b25d3fc1d63c8ee465f
Tony <tonyshou1000@gmail.com>
2021-04-07 17:20:02 +0800

    1.0

---
+++ 帳密練習.py
@@ -0,0 +1,11 @@
+
+
+while True:
+    account=input("請輸入帳號:")
+    password=input("請輸入密碼:")
+    if (account!="abc") or (password!="123"):
+        print("帳號不符!請重新輸入!")
+    else:
+        print("歡迎光臨")
+        break
+    
