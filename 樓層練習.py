50532bc1beef1a85fd9aa55b260ca6584ae81c58
Tony <tonyshou1000@gmail.com>
2021-04-09 00:14:47 +0800

    3.0

--- 樓層練習.py
+++ 樓層練習.py
@@ -2,11 +2,13 @@ f=eval(input("請輸入大樓的樓層數:"))
 print("本大樓樓層列表:")
 i=1
 if f<4: 
-    for i in range(1,f+1):
-        print("{}樓".format(i))
+    f=f+1
 else:
-    for i in range(1,f+2):
-        if i==4:
-            continue
-        print("{}樓".format(i))
-    
+    f=f+2
+
+
+
+for i in range(1,f):
+    if i==4:
+        continue
+    print("{}樓".format(i))
